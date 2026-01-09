f39be816f332fc252a20f2387832bef0fc753ca9
ighalo-north <renighalo@gmail.com>
2026-01-09 14:48:47 -0500

    import data in script

---
+++ assignment1.R
@@ -0,0 +1,7 @@
+#input some piece of data into R and do a substantiative calculation
+
+#this script reads a spreadsheet and does a calculation
+
+#read in the spreadsheet
+library(readxl)
+AS_ALANR_jan9 <- read_excel("AS_ALANR_jan9.xlsx")
